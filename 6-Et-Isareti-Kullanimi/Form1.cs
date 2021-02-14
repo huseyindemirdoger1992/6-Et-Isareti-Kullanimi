c0e31ebaf48cc9574c870251a45a035a77addffc
Hüseyin DEMİRDÖĞER <huseyindemirdoger1992@hotmail.com>
2021-02-14 07:17:20 +0300

    Add project files.

---
+++ 6-Et-Isareti-Kullanimi/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _6_Et_Isareti_Kullanimi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // string HataliKullanim = "Hüseyin \ DEMİRDÖĞER";
+
+            string Cozum1 = "Hüseyin \\ DEMİRDÖĞER";
+
+            string Cozum2 = @"Hüseyin \ DEMİRDÖĞER";
+
+        }
+    }
+}
